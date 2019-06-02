14310b0d20380d50c5bba2bf576f547ae87abc96
Jay Li <jayli3n@gmail.com>
2019-06-02 17:43:41 +0800

    wrap up

--- src/components/header.js
+++ src/components/header.js
@@ -18,6 +18,7 @@ const styles = {
 		alignItems: 'center',
 		setHeight: 60,
 		paddingTop: 45,
+		paddingBottom: 6,
 		backgroundColor: '#F8F8F8',
 		shadowColor: '#000',
 		shadowOffset: { width: 0, height: 2 },
