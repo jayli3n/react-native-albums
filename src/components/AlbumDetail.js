9de3107c51c00064edbafd656aa4a6916d346159
Jay Li <jayli3n@gmail.com>
2019-06-02 16:28:23 +0800

    added styled sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
 	const { title, artist, url, image, thumbnail_image } = props.album;
 
 	return (
-		<View>
-			<Text>{title}</Text>
-			<Text>{artist}</Text>
-			<Text>{url}</Text>
-			<Text>{image}</Text>
-			<Text>{thumbnail_image}</Text>
-		</View>
+		<Card>
+			<CardSection>
+				<Text>{title}</Text>
+			</CardSection>
+			<CardSection>
+				<Text>{artist}</Text>
+			</CardSection>
+			<CardSection>
+				<Text>{url}</Text>
+			</CardSection>
+			<CardSection>
+				<Text>{image}</Text>
+			</CardSection>
+			<CardSection>
+				<Text>{thumbnail_image}</Text>
+			</CardSection>
+		</Card>
 	);
 };
 
